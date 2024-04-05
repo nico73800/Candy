ca609b53ba519dc0819a107f56d717a2509db626
nico73800 <nico.chalo@outlook.fr>
2024-04-05 10:55:15 +0200

    ajout de la partie Statistiques

---
+++ Candy/Candy/StatsView.swift
@@ -0,0 +1,14 @@
+//
+//  StatsModel.swift
+//  Candy
+//
+//  Created by nicolas chaloyard on 28/03/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Statistiques {
+    @Attribute(.unique) var 
+}
